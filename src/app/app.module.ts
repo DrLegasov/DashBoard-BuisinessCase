3a426effaf805e4ef11d1692ee1ab58c4784ca61
Christophe ROSSI <crossi@stagiaire-humanbooster.com>
2021-09-17 11:28:59 +0200

    création dashB

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/header/header.component';
+import { ErrorsFormComponent } from './components/errors-form/errors-form.component';
+import { AuthViewComponent } from './views/auth-view/auth-view.component';
+import { DashViewComponent } from './views/dash-view/dash-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ErrorsFormComponent,
+    AuthViewComponent,
+    DashViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
